3f11822ffdd18e61007375463cfcbdafce454e70
Chaeyeon LEE <amy01051077@gmail.com>
2025-04-14 01:15:11 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+-- 처음엔 안했는데, 여기 와서 중성화를 한 동물 찾기
+select animal_id, animal_type, name
+from animal_outs
+where 
+    (SEX_UPON_outcome like 'Spayed%' or SEX_UPON_outcome like 'Neutered%')
+    and animal_id in 
+    (SELECT i.animal_id
+    from animal_ins as i
+    left join animal_outs as o
+    on i.animal_id = o.animal_id
+    where i.sex_upon_intake != o.sex_upon_outcome)
